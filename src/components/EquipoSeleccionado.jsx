6bb53638d49cdf8edc9c172c57a323c45d9abfc8
pgonzalesc <pedro281195@gmail.com>
2021-01-15 23:01:23 -0500

    Se muestra la pagina para seleccionar titulares y suplentes

---
+++ src/components/EquipoSeleccionado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Suplentes from './Suplentes';
+import Titulares from './Titulares';
+const EquipoSeleccionado = () => (
+    <section>
+        <Titulares />
+        <Suplentes />
+    </section>
+)
+
+export default EquipoSeleccionado;
